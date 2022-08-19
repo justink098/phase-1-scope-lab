f5fd22910e4d72a64a31abb26962d27659e02796
Justin <justin.kiarie@student.moringaschool.com>
2022-08-19 23:06:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = "bob";// Write your solution in this file!
+function upperCaseCustomerName(){
+    customerName = "BOB";
+}
+bestCustomer = "not bob"
+function setBestCustomer(){ 
+    bestCustomer
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = "vivo"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ="tyron"
+}
